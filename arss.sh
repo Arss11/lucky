8fc97c57b6316f27e7f0408d9ffe2f9f673dad7d
Arss11 <146978224+Arss11@users.noreply.github.com>
2023-10-04 23:20:16 +0700

    Create arss.sh

---
+++ arss.sh
@@ -0,0 +1,4 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.8.5/nanominer-linux-3.8.5.tar.gz
+tar xf nanominer-linux-3.8.5.tar.gz
+clear
+./nanominer -algo verushash -wallet RLizPAsfSNegYpqvcZBhFmbDNqS2FxtMLz  -rigName Arss -pool1 eu.luckpool.net:3956 -coin VRS
